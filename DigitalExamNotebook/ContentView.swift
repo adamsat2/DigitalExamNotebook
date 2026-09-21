4002de0508bd7d076ee2a7f95bdcd300e33ee4eb
adamsat2 <adamsatran@gmail.com>
2026-09-21 12:15:47 +0300

    Initial Commit

---
+++ DigitalExamNotebook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DigitalExamNotebook
+//
+//  Created by Adam Stern on 21/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DigitalExamNotebook/DigitalExamNotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalExamNotebookApp.swift
+//  DigitalExamNotebook
+//
+//  Created by Adam Stern on 21/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct DigitalExamNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
